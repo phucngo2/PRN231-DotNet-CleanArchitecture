53228290232e299a184eccc6b4d38be9b37c7a4e
phucngo2 <phucnnh21@gmail.com>
2024-07-15 21:23:36 +0700

    feature(37): Status Controller

---
+++ PRN231.API/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN231.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StatusController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello, 世界!");
+    }
+}
